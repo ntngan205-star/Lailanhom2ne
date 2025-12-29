14e9f00ab5b4875b14b9db561011670e301267de
linhaanini <jasnilyn00001010@gmail.com>
2025-12-29 07:25:22 +0700

    Add files via upload

---
+++ nhom2huongdan/nhom2huongdan/khachhang.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace nhom2huongdan
+{
+    partial class frmKhachHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTimKiem = new System.Windows.Forms.TextBox();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.MaKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CCCD = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoDienThoai = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.Location = new System.Drawing.Point(781, 40);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(136, 64);
+            this.btnXoa.TabIndex = 14;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Tìm kiếm theo tên:";
+            // 
+            // txtTimKiem
+            // 
+            this.txtTimKiem.Location = new System.Drawing.Point(55, 57);
+            this.txtTimKiem.Multiline = true;
+            this.txtTimKiem.Name = "txtTimKiem";
+            this.txtTimKiem.Size = new System.Drawing.Size(297, 47);
+            this.txtTimKiem.TabIndex = 12;
+            this.txtTimKiem.TextChanged += new System.EventHandler(this.txtTimKiem_TextChanged);
+            // 
+            // btnSua
+            // 
+            this.btnSua.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnSua.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.Location = new System.Drawing.Point(606, 40);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(136, 64);
+            this.btnSua.TabIndex = 11;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnThem
+            // 
+            this.btnThem.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThem.Location = new System.Drawing.Point(432, 40);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(136, 64);
+            this.btnThem.TabIndex = 10;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaKH,
+            this.TenKH,
+            this.CCCD,
+            this.GioiTinh,
+            this.DiaChi,
+            this.SoDienThoai});
+            this.dgvKhachHang.Location = new System.Drawing.Point(55, 142);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.RowHeadersWidth = 62;
+            this.dgvKhachHang.RowTemplate.Height = 28;
+            this.dgvKhachHang.Size = new System.Drawing.Size(862, 506);
+            this.dgvKhachHang.TabIndex = 9;
+            // 
+            // MaKH
+            // 
+            this.MaKH.DataPropertyName = "MaKH";
+            this.MaKH.HeaderText = "Mã khách hàng";
+            this.MaKH.MinimumWidth = 8;
+            this.MaKH.Name = "MaKH";
+            // 
+            // TenKH
+            // 
+            this.TenKH.DataPropertyName = "TenKH";
+            this.TenKH.HeaderText = "Tên khách hàng";
+            this.TenKH.MinimumWidth = 8;
+            this.TenKH.Name = "TenKH";
+            // 
+            // CCCD
+            // 
+            this.CCCD.DataPropertyName = "CCCD";
+            this.CCCD.HeaderText = "CCCD";
+            this.CCCD.MinimumWidth = 8;
+            this.CCCD.Name = "CCCD";
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.HeaderText = "Giới tính";
+            this.GioiTinh.MinimumWidth = 8;
+            this.GioiTinh.Name = "GioiTinh";
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.HeaderText = "Địa chỉ";
+            this.DiaChi.MinimumWidth = 8;
+            this.DiaChi.Name = "DiaChi";
+            // 
+            // SoDienThoai
+            // 
+            this.SoDienThoai.DataPropertyName = "SoDienThoai";
+            this.SoDienThoai.HeaderText = "Số điện thoại";
+            this.SoDienThoai.MinimumWidth = 8;
+            this.SoDienThoai.Name = "SoDienThoai";
+            // 
+            // frmKhachHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(967, 670);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTimKiem);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Name = "frmKhachHang";
+            this.Text = "Khách hàng";
+            this.Load += new System.EventHandler(this.frmKhachHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTimKiem;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CCCD;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoDienThoai;
+    }
+}
+
---
+++ nhom2huongdan/nhom2huongdan/ttinkhachhang.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nhom2huongdan
+{
+    public partial class frmTTinKhachHang : Form
+    {
+        public frmTTinKhachHang()
+        {
+            InitializeComponent();
+        }
+
+        KetNoiDuLieu kn = new KetNoiDuLieu();
+        SqlCommand cmd = null;
+
+        private void frmTTinKhachHang_Load(object sender, EventArgs e)
+        {
+            kn.Myconn();
+
+            //Phải cho txt sửa lên tren form_load vs sửa đc
+            txtMaKhachHang.Text = StrgMa;
+            txtTenKhachHang.Text = StrgTen;
+            txtCCCD.Text = StrgCCCD;
+            txtGioiTinh.Text = StrgGT;
+            txtDiaChi.Text = StrgDC;
+            txtSoDienThoai.Text = StrgSDT;
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlThem = "insert into KhachHang (MaKH, TenKH, CCCD,GioiTinh,DiaChi,SoDienThoai ) values (@MKH,@TKH,@CCCD,@GT,@DC,@SDT)";
+                cmd = new SqlCommand(sqlThem, kn.conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add("@MKH", SqlDbType.Int).Value = txtMaKhachHang.Text.Trim();
+                cmd.Parameters.Add("@TKH", SqlDbType.NVarChar).Value = txtTenKhachHang.Text.Trim();
+                cmd.Parameters.Add("@CCCD", SqlDbType.Int).Value = txtCCCD.Text.Trim();
+                cmd.Parameters.Add("@GT", SqlDbType.Int).Value = txtGioiTinh.Text.Trim();
+                cmd.Parameters.Add("@DC", SqlDbType.NVarChar).Value = txtDiaChi.Text.Trim();
+                cmd.Parameters.Add("@SDT", SqlDbType.Int).Value = txtSoDienThoai.Text.Trim();
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("thêm thành công");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("thêm thất bại");
+                throw;
+            }
+            
+        }
+
+        public frmTTinKhachHang(string strgMa, string strgTen, string strgCCCD, string strgGT, string strgDC, string strgSDT)
+        {
+            InitializeComponent();
+            this.StrgMa = strgMa;
+            this.StrgTen = strgTen;
+            this.StrgCCCD = strgCCCD;
+            this.StrgGT = strgGT;
+            this.StrgDC = strgDC;
+            this.StrgSDT = strgSDT;
+        }
+
+        public string StrgMa { get; set; }
+
+        public string StrgTen { get; set; }
+        
+        public string StrgCCCD { get; set; }
+        public string StrgGT { get; set; }
+        public string StrgDC { get; set; }
+        public string StrgSDT { get; set; }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string sqlSua = "update KhachHang set MaKH=@MKH, TenKH=@TKH, CCCD=@CCCD,GioiTinh=@GT, DiaChi=@DC,SoDienThoai=@SDT  where MaKH=@MKH";
+
+                cmd = new SqlCommand(sqlSua, kn.conn);
+                cmd.Parameters.Clear();
+                cmd.Parameters.Add("@MKH", SqlDbType.Int).Value = txtMaKhachHang.Text.Trim();
+                cmd.Parameters.Add("@TKH", SqlDbType.NVarChar).Value = txtTenKhachHang.Text.Trim();
+                cmd.Parameters.Add("@CCCD", SqlDbType.Int).Value = txtCCCD.Text.Trim();
+                cmd.Parameters.Add("@GT", SqlDbType.Int).Value = txtGioiTinh.Text.Trim();
+                cmd.Parameters.Add("@DC", SqlDbType.NVarChar).Value = txtDiaChi.Text.Trim();
+                cmd.Parameters.Add("@SDT", SqlDbType.Int).Value = txtSoDienThoai.Text.Trim();
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("sửa thành công");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("sửa thất bại");
+                throw;
+            }
+            
+        }
+    }
+}
